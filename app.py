96528cef865abe8fe2186fd52b4c6bc3704f7b7a
balbino aylagas <balbino_a@hotmail.com>
2020-07-17 19:08:14 -0600

    add html modify app

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("message")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("index.html")
+    else:  # "POST"
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
