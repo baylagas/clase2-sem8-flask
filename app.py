abbc9944e23443a0176d728c293135f0d23d3aeb
balbino aylagas <balbino_a@hotmail.com>
2020-07-17 19:20:18 -0600

    modify app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from alumnologic import AlumnoLogic
 
 app = Flask(__name__)
 
@@ -8,7 +9,12 @@ def index():
     if request.method == "GET":
         return render_template("index.html")
     else:  # "POST"
-        return render_template("index.html")
+        name = request.form["name"]
+        age = request.form["age"]
+        logic = AlumnoLogic()
+        rows = logic.insertNewAlumno(name, age)
+        message = f"{rows} affected"
+        return render_template("index.html", message=message)
 
 
 if __name__ == "__main__":
